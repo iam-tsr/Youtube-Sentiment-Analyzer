bc5520d3966e6b2bed787c7479548339c1530ec8
Harshit Saini <146534460+sainiharshit322@users.noreply.github.com>
2025-07-11 18:19:21 +0530

    Update History.js

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -7,7 +7,7 @@ export default function History() {
   useEffect(() => {
     const fetchHistory = async () => {
       try {
-        const res = await fetch("http://127.0.0.1:8000/history", {
+        const res = await fetch("youtube-sentiment-analyzer-production.up.railway.app/history", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
           },
